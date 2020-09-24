966280fdb357ba67b8770792dc124adb52428218
Glaston <glaston@astrumlabs.co>
2020-09-24 15:28:48 +0530

    feat(Section 4 (CRUD funtionality)): :sparkles: End of Section 4CRUD Functionality complete. Removed all the unnecessary using statements.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -22,24 +22,37 @@ namespace API.Controllers
             _mediator = mediator;
         }
 
+
+        // @desc    Get Activities.
+        // @route   GET /api/activities
+        // @access  Public
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> List()
         {
             return await _mediator.Send(new List.Query());
         }
 
+        // @desc    Get Activity.
+        // @route   GET /api/activities/:id
+        // @access  Public
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Details(Guid id)
         {
             return await _mediator.Send(new Details.Query { Id = id });
         }
 
+        // @desc    Create Activity.
+        // @route   POST /api/activities
+        // @access  Public
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await _mediator.Send(command);
         }
 
+        // @desc    Edit Activity.
+        // @route   PUT /api/activities/:id
+        // @access  Public
         [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
@@ -47,6 +60,9 @@ namespace API.Controllers
             return await _mediator.Send(command);
         }
 
+        // @desc    Delete Activity.
+        // @route   DELETE /api/activities/:id
+        // @access  Public
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
